6f2deadcf9972c3892c4c732d8cc15b043e941be
Govind S Nair <115881229+Govind-S-Nair@users.noreply.github.com>
2023-05-24 18:15:46 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import Tweet from "./Tweet";
 
 function App(){
+  const [users, setUsers] = useState([
+    { name: "Govind S Nair", message: "Learning React is fun!"},
+    { name: "Haridev Prasad", message: "Crafting stuff using Blender."},
+    { name: "Naarasim Krishna", message: "Checking out a new Star Wars game!"},
+    { name: "Ram Sankar", message: "Building a Java GUI using AWT."}
+  ]);
   return(
     <div className="app">
-      <Tweet name="Govind S Nair" message="Learning React is fun!" likes="Likes: 30"/>
-      <Tweet name="Haridev Prasad" message="Crafting stuff using Blender." likes="Likes: 32"/>
-      <Tweet name="Naarasim Krishna" message="Checking out a new Star Wars game!" likes="Likes: 66"/>
-      <Tweet name="Ram Sankar" message="Building a Java GUI using AWT." likes="Likes: 38"/>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
